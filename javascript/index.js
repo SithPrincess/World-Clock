0e8069afae1f6624e64c3f78974ee8ef888f0631
SithPrincess <Cassie2727@gmail.com>
2025-03-03 02:23:46 -0600

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -20,6 +20,9 @@ function updateTime() {
 
 function cityUpdate(event) {
   let cityTimeZone = event.target.value;
+        if(cityTimeZone === "current"){
+            cityTimeZone = moment.tz.guess();
+        }
   let cityName= cityTimeZone.replace("_"," ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
